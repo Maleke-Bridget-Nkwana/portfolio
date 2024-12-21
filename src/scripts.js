cfc749f3ab2c674fa940892b6216ae5c3b868234
Maleke-Bridget-Nkwana <166614093+Maleke-Bridget-Nkwana@users.noreply.github.com>
2024-12-21 20:40:58 +0200

    Added enhancements

--- src/scripts.js
+++ src/scripts.js
@@ -25,3 +25,12 @@ themeToggle.addEventListener("click", () => {
   }
 });
 
+// Smooth scrolling for navigation links
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth",
+    });
+  });
+});
